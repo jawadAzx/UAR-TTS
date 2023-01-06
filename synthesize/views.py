3e602201f483151cc95f6e7b0d70c909188c09cc
jawadAzx <23100321@lums.edu.pk>
2023-01-06 21:41:20 +0500

    made changes to view.py

--- synthesize/views.py
+++ synthesize/views.py
@@ -8,18 +8,21 @@ import json
 def synthesize(request):
     if request.method == 'POST':
         modelFolder_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'synthesize', 'model')
-        configPath = os.path.join(modelFolder_path, 'config.json')
-        modelPath = os.path.join(modelFolder_path, 'checkpoint_430000.pth')
-        outPath = os.path.join(modelFolder_path, 'test.wav')
+        configPath = "config.json"
+        modelPath = "checkpoint_430000.pth"
+        outPath = "test.wav"
         data = request.body.decode('utf-8')
         data = json.loads(data)
         text = data['text']
-        os.system("tts --text " + text + " --config_path " + configPath + " --model_path " + modelPath + " --out_path " + outPath)
-        with open(outPath, 'rb') as f:
-            response = HttpResponse(f.read(), content_type='audio/wav')
-            response['Content-Disposition'] = 'attachment; filename="test.wav"'
-            os.remove(outPath)
+        try:
+            os.system("tts --text " + text + " --config_path " + configPath + " --model_path " + modelPath + " --out_path " + outPath)
+            with open(outPath, 'rb') as f:
+                response = HttpResponse(f.read(), content_type='audio/wav')
+                response['Content-Disposition'] = 'attachment; filename="test.wav"'
+                os.remove(outPath)
             return response
+        except:
+            return HttpResponse("Error")
     else:
         return HttpResponse("Not a GET request")
 
