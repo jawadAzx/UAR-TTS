58826953b9a71b27f8e5ee3b3a6f4db5edc68bee
jawadAzx <23100321@lums.edu.pk>
2023-01-07 01:42:06 +0500

    implemented subprocess instead of os

--- synthesize/views.py
+++ synthesize/views.py
@@ -3,19 +3,20 @@ from django.views.decorators.csrf import csrf_exempt
 from django.http import HttpResponse
 import os
 import json
+import subprocess
 @csrf_exempt
 # Create your views here.
 def synthesize(request):
     if request.method == 'POST':
-        modelFolder_path = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), 'synthesize', 'model')
         configPath = "config.json"
         modelPath = "checkpoint_430000.pth"
         outPath = "test.wav"
         data = request.body.decode('utf-8')
         data = json.loads(data)
         text = data['text']
+        command = "tts --text " + text + " --config_path " + configPath + " --model_path " + modelPath + " --out_path " + outPath
         try:
-            os.system("tts --text " + text + " --config_path " + configPath + " --model_path " + modelPath + " --out_path " + outPath)
+            subprocess.call(command, shell=True)
             with open(outPath, 'rb') as f:
                 response = HttpResponse(f.read(), content_type='audio/wav')
                 response['Content-Disposition'] = 'attachment; filename="test.wav"'
